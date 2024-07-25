39735846c235ba163c1028098eaa33d690b5f9a7
Aditya Kumar <adityarazz3495@gmail.com>
2024-07-26 04:48:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+
+const image = document.getElementsByTagName('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+
+console.log(image);
+const img = image[0];
+console.log(img)
+// Music
+const songs = [
+  {
+    name: 'sajni',
+    displayName: 'Sajni-re',
+    artist: 'Arjit singh',
+  },
+  {
+    name: 'tere liye',
+    displayName: 'Tere Liye',
+    artist: 'Lata Mangeshkar',
+  },
+  {
+    name: 'atif-mashup',
+    displayName: 'Woh Lamhe Woh Baatein',
+    artist: 'Atif Aslam',
+  },
+  {
+    name: 'dua kro',
+    displayName: 'Dua Karo',
+    artist: 'Sachin-Jiger',
+  },
+  {
+    name: 'ek-villan',
+    displayName: 'Galliyan',
+    artist: 'Ankit Tiwari',
+  },
+  {
+    name: 'khairiyat',
+    displayName: 'Khairiyat',
+    artist: 'Arjit singh',
+  },
+  {
+    name: 'tu hai ki nhi',
+    displayName: 'Tu-Hai-Ki-Nhi',
+    artist: 'Ankit and Tulsi Kumar',
+  },
+  {
+    name: 'zaruri tha',
+    displayName: 'Zaroori-Tha',
+    artist: 'Fateh Ali Khan',
+  },
+  {
+    name: 'dil tod ke',
+    displayName: 'Dil tod ke',
+    artist: 'B Praak',
+  },
+  {
+    name: 'atif-mashup',
+    displayName: 'Atif Mashup',
+    artist: 'Atif Aslam',
+  },
+];
+
+// Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `${song.displayName}.mp3`;
+  img.src=`${song.name}.jpg`;
+  
+
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    // Update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    // Calculate display for currentTime
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
